4d34c7ed365a35cdb446e518a3e57acad6dc94aa
gowtham15012006 <gowtham88044@gmail.com>
2026-04-09 22:23:11 +0530

    remover feature

--- userinput.java
+++ userinput.java
@@ -5,7 +5,6 @@ public class userinput {
         System.out.print("enter your name:");
         String name = sc.nextLine();
         System.out.println("Hello " + name);
-        System.out.println("hai " +name " how are you");
     }
     
 }
