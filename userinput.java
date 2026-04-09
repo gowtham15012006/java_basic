aa6b0ebe1f4aaabd3cb671b8272ae316f627787e
Gowtham <gowtham88044@gmail.com>
2026-04-09 22:20:19 +0530

    add new feature

--- userinput.java
+++ userinput.java
@@ -5,6 +5,7 @@ public class userinput {
         System.out.print("enter your name:");
         String name = sc.nextLine();
         System.out.println("Hello " + name);
+        System.out.println("hai " +name " how are you");
     }
     
 }
